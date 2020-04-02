b5ec8effd0c90a2e7963ef25f8ffdff1ecb9984f
Aleksandr Fida <aleksandr_fida@epam.com>
2020-04-02 14:07:07 +0400

    add retinanet handler

--- multi_person_tracker/mpt.py
+++ multi_person_tracker/mpt.py
@@ -70,8 +70,8 @@ class MPT():
         self.detection_threshold = detection_threshold
         self.output_format = output_format
         self.detector_type = detector_type
-        self.detector_checkpoint=detector_checkpoint,
-        self.detector_config=detector_config
+        self.detector_checkpoint=detector_checkpoint[0],
+        self.detector_config=detector_config[0]
 
         if self.detector_type == 'maskrcnn':
             self.detector = keypointrcnn_resnet50_fpn(pretrained=True).to(self.device).eval()
@@ -85,8 +85,6 @@ class MPT():
             # x = torch.Tensor([np.random.rand(3, 300, 400), np.random.rand(3, 300, 400)])
             # print(self.detector(x))
         elif self.detector_type == 'retina':
-            print(self.detector_config)
-            print(self.detector_checkpoint)
             self.detector = init_detector(
                 self.detector_config, self.detector_checkpoint, device='cuda:0'
             )
